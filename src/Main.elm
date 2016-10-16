c558ec450fc1d7930a2788a438a46114b0497cf1
rupert <rupert.voelcker@bt.com>
2016-10-16 14:18:49 +0100

    RenderString: show a basic string to be typed

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,53 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (Html)
+import Html.App
 
 
 main =
-    Html.text "Typo a no no"
+    Html.App.program
+        { init = init
+        , subscriptions = (\_ -> Sub.none)
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { text : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { text = "Something to type"
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.text model.text ]
