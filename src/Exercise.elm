837b9ef49f5de7823906424b2d952de5fef2bb66
rupert <rupert.voelcker@bt.com>
2016-10-19 23:52:25 +0100

    Tree: switch in a tree zipper (still have to use it to improve code)

--- src/Exercise.elm
+++ src/Exercise.elm
@@ -3,8 +3,8 @@ module Exercise exposing (Exercise, accuracy, consume, init, isComplete, steps)
 import Char
 import Html exposing (Html)
 import Html.Attributes
-import List.Zipper as Zipper exposing (Zipper)
-import SafeZipper
+import MultiwayTree exposing (Tree(..))
+import MultiwayTreeZipper as Zipper exposing (Zipper)
 import Step exposing (Step, Direction(..), Status(..))
 import String
 
@@ -14,7 +14,7 @@ backspaceChar =
 
 
 type alias Exercise =
-    { steps : Maybe (Zipper Step)
+    { steps : Zipper Step
     , typedCharacterCount : Int
     }
 
@@ -22,21 +22,25 @@ type alias Exercise =
 init : String -> Exercise
 init source =
     { steps =
-        source
+        ( source
             |> String.split ""
             |> List.map Step.init
-            |> flip List.append [ Step.end ]
-            |> Zipper.fromList
-            |> Maybe.map setCurrentStatus
+            |> (List.foldr (\s t -> Tree s [ t ]) (Tree Step.end []))
+        , []
+        )
+            |> setCurrentStatus
     , typedCharacterCount = 0
     }
+
 
 
 steps : Exercise -> List Step
 steps exercise =
     exercise.steps
-        |> Maybe.map Zipper.toList
-        |> Maybe.withDefault []
+        |> Zipper.goToRoot
+        |> (Maybe.withDefault exercise.steps)
+        |> fst
+        |> MultiwayTree.flatten
 
 
 consume : Char -> Exercise -> Exercise
@@ -44,9 +48,9 @@ consume char exercise =
     { exercise
         | steps =
             exercise.steps
-                |> Maybe.map (updateCurrentStep char)
-                |> Maybe.map moveZipper
-                |> Maybe.map setCurrentStatus
+                |> updateCurrentStep char
+                |> moveZipper
+                |> setCurrentStatus
         , typedCharacterCount = addCharacter char exercise.typedCharacterCount
     }
 
@@ -54,10 +58,10 @@ consume char exercise =
 isComplete : Exercise -> Bool
 isComplete exercise =
     exercise.steps
-        |> Maybe.map Zipper.current
-        |> Maybe.map .status
-        |> Maybe.map ((==) End)
-        |> Maybe.withDefault False
+        |> Zipper.datum
+        |> .status
+        |> ((==) End)
+
 
 
 accuracy : Exercise -> Float
@@ -70,18 +74,54 @@ accuracy exercise =
             * 100
 
 
-
 -- PRIVATE FUNCTIONS
-
 
 exerciseCharacterCount : Exercise -> Int
 exerciseCharacterCount exercise =
-    exercise.steps
-        |> Maybe.map Zipper.toList
-        |> Maybe.map List.length
-        |> Maybe.map (flip (-) 1)
-        |> Maybe.withDefault 0
+    exercise
+        |> steps
+        |> List.length
+        |> ((+) -1)
 
+
+setCurrentStatus : Zipper Step -> Zipper Step
+setCurrentStatus steps =
+    steps
+        |> Zipper.updateDatum Step.makeCurrent
+        |> Maybe.withDefault steps
+
+
+updateCurrentStep : Char -> Zipper Step -> Zipper Step
+updateCurrentStep char steps =
+    Zipper.updateDatum (Step.consume char) steps
+        |> Maybe.withDefault steps
+
+
+moveZipper : Zipper Step -> Zipper Step
+moveZipper steps =
+    let
+        mover =
+            steps
+                |> Zipper.datum
+                |> .moveTo
+                |> zipperMover
+    in
+        steps
+            |> mover
+            |> Maybe.withDefault steps
+
+
+zipperMover : Direction -> (Zipper Step -> Maybe (Zipper Step))
+zipperMover direction =
+    case direction of
+        Next ->
+            Zipper.goToChild 0
+
+        Previous ->
+            Zipper.goUp
+
+        _ ->
+            Just
 
 addCharacter : Char -> Int -> Int
 addCharacter char currentCount =
@@ -89,36 +129,3 @@ addCharacter char currentCount =
         currentCount
     else
         currentCount + 1
-
-
-updateCurrentStep : Char -> Zipper Step -> Zipper Step
-updateCurrentStep char steps =
-    Zipper.update (Step.consume char) steps
-
-
-moveZipper : Zipper Step -> Zipper Step
-moveZipper steps =
-    steps
-        |> (steps
-                |> Zipper.current
-                |> .moveTo
-                |> zipperMover
-           )
-
-
-setCurrentStatus : Zipper Step -> Zipper Step
-setCurrentStatus steps =
-    Zipper.update Step.makeCurrent steps
-
-
-zipperMover : Direction -> (Zipper Step -> Zipper Step)
-zipperMover direction =
-    case direction of
-        Next ->
-            SafeZipper.next
-
-        Previous ->
-            SafeZipper.previous
-
-        _ ->
-            identity
