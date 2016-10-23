0703bd1f1787087286c7760d473773b09765f96d
rupert <rupert.voelcker@bt.com>
2016-10-23 12:18:17 +0100

    Parser: parse caharacters with optional leading whitespace

--- src/ExerciseParser.elm
+++ src/ExerciseParser.elm
@@ -1,7 +1,33 @@
-module ExerciseParser exposing (leadingWhitepace)
+module ExerciseParser exposing (character, leadingWhitepace, toSteps)
 
-import Combine exposing (Parser, map, regex)
+import Combine exposing (Parser, choice, count, many, map, regex, sequence)
 import Step exposing (Step)
+
+
+-- prob make sense to add a \n to the end of the string
+-- and use manyTill the end of line to parse each line
+
+
+toSteps : Parser (List Step)
+toSteps =
+    choice
+        [ whitespaceAndCharacters
+        , many character
+        ]
+
+
+whitespaceAndCharacters : Parser (List Step)
+whitespaceAndCharacters =
+    sequence
+        [ count 1 leadingWhitepace
+        , many character
+        ]
+        |> map (\l -> List.concatMap identity l)
+
+
+character : Parser Step
+character =
+    map Step.init (regex ".")
 
 
 leadingWhitepace : Parser Step
--- tests/TestExerciseParser.elm
+++ tests/TestExerciseParser.elm
@@ -10,10 +10,41 @@ import Test exposing (..)
 exerciseParser : Test
 exerciseParser =
     describe "ExerciseParser"
-        [ describe "testing"
-            [ test "parses whitespace" <|
+        [ describe "basic parsers"
+            [ test "whitespace" <|
                 \() ->
                     parse ExerciseParser.leadingWhitepace "   "
                         |> Expect.equal ( Ok (Step.skip "   "), { input = "", position = 3 } )
+            , test "non-whitespace" <|
+                \() ->
+                    parse ExerciseParser.character "ab"
+                        |> Expect.equal ( Ok (Step.init "a"), { input = "b", position = 1 } )
+            ]
+        , describe "toSteps"
+            [ test "parses string without leading whitespace" <|
+                \() ->
+                    parse ExerciseParser.toSteps "ab c"
+                        |> Expect.equal
+                            ( Ok
+                                [ Step.init "a"
+                                , Step.init "b"
+                                , Step.init " "
+                                , Step.init "c"
+                                ]
+                            , { input = "", position = 4 }
+                            )
+            , test "parses string with leading whitespace" <|
+                \() ->
+                    parse ExerciseParser.toSteps "   ab c"
+                        |> Expect.equal
+                            ( Ok
+                                [ Step.skip "   "
+                                , Step.init "a"
+                                , Step.init "b"
+                                , Step.init " "
+                                , Step.init "c"
+                                ]
+                            , { input = "", position = 7 }
+                            )
             ]
         ]
