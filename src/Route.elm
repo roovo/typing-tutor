481edb30d3c065918585ce2325e247f767910ddc
rupert <rupert.voelcker@bt.com>
2016-11-08 19:45:46 +0000

    Cleanup: move changeable parts of Routes.elm to the top

--- src/Route.elm
+++ src/Route.elm
@@ -6,28 +6,6 @@ import Hop
 import Hop.Types as Hop
 
 
-hopConfig : Hop.Config
-hopConfig =
-    { hash = False
-    , basePath = ""
-    }
-
-
-type Route
-    = ExerciseListRoute
-    | ExerciseRoute Int
-    | NotFoundRoute
-
-
-routes : UrlParser.Parser (Route -> a) a
-routes =
-    UrlParser.oneOf
-        [ UrlParser.format ExerciseListRoute (UrlParser.s "")
-        , UrlParser.format ExerciseRoute (UrlParser.s "exercises" </> UrlParser.int)
-        , UrlParser.format ExerciseListRoute (UrlParser.s "exercises")
-        ]
-
-
 urlFor : Route -> String
 urlFor route =
     case route of
@@ -39,6 +17,28 @@ urlFor route =
 
         NotFoundRoute ->
             "/404"
+
+
+routes : UrlParser.Parser (Route -> a) a
+routes =
+    UrlParser.oneOf
+        [ UrlParser.format ExerciseListRoute (UrlParser.s "")
+        , UrlParser.format ExerciseRoute (UrlParser.s "exercises" </> UrlParser.int)
+        , UrlParser.format ExerciseListRoute (UrlParser.s "exercises")
+        ]
+
+
+hopConfig : Hop.Config
+hopConfig =
+    { hash = False
+    , basePath = ""
+    }
+
+
+type Route
+    = ExerciseListRoute
+    | ExerciseRoute Int
+    | NotFoundRoute
 
 
 urlParser : Navigation.Parser ( Route, Hop.Address )
