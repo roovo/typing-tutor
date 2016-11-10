acb720ecaa38a6da2cf724de3710f22bfadd7afc
rupert <rupert.voelcker@bt.com>
2016-11-10 20:31:01 +0000

    Chart: line chart with a time axis

--- dist/ports.js
+++ dist/ports.js
@@ -20,39 +20,54 @@ document.onkeypress = function(e) {
   }
 }
 
+function newDate(epoc) {
+  return moment(epoc).toDate();
+}
+
+var timeFormat = 'MM/DD/YYYY HH:mm';
+
 var ctx = document.getElementById("myChart");
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'WPM',
+            radius: 5,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            data: [{
+                x: newDate(1478606177234),
+                y: 30
+            }, {
+                x: newDate(1478706177234),
+                y: 35
+            }, {
+                x: newDate(1478708177234),
+                y: 33
+            // }, {
+            // label: 'Accuracy',
+            // radius: 5,
+            // backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            // data: [{
+            //     x: newDate(1478606177234),
+            //     y: 99.7
+            // }, {
+            //     x: newDate(1478706177234),
+            //     y: 90.456
+            // }, {
+            //     x: newDate(1478708177234),
+            //     y: 94.34
+            }]
         }]
     },
     options: {
+        showLines: false,
+        responsive: false,
+        maintainAspectRatio: false,
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+            xAxes: [{
+                type: 'time',
+                position: 'bottom'
             }]
         }
     }
